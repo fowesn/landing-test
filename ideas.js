0c703c174d292426245b37b473c68e5521a0e376
fowesn <fowesn@bk.ru>
2022-02-01 21:06:17 +0300

    Create ideas.js

---
+++ ideas.js
@@ -0,0 +1,15 @@
+const ideas = [
+  'покушать', 
+  'поспать', 
+  'погладить кошку', 
+  'попить вино',
+  'погулять',
+  'написать незнакомому человеку',
+  'заварить кофе',
+  'найти место для хорошей фотографии',
+  'почитать',
+  'написать лучший в мире код',
+  'поиграть на фортепиано',
+  'порисовать',
+  'много текста. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+];
